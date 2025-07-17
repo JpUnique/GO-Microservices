b5607cc2ab67f6a843c1896b8f938a6ff81e81b9
JpUnique <jpunique1234@gmail.com>
2025-07-17 15:04:23 +0100

    Go's design philosophy and feature set make it an exceptionally strong choice for building robust, scalable, and maintainable microservice architectures.

---
+++ account_resolver.go
@@ -0,0 +1,12 @@
+package main
+
+
+type accountResolver struct{
+	server *Server
+}
+
+
+
+// func (r *accountResolver) Orders(ctx context.Context, pagination *PaginationInput, obj *Account)([]*Order, error){
+
+// }
